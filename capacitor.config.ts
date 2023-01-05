af308b86ecb589c268db26c338ab8f7e98d98d58
Matheus Aguiar <matheustxaguiar@gmail.com>
2023-01-05 15:27:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'recicla-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
